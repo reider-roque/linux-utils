a873f48c3f18874ba2be3f89b5d95279a6ad157d
Oleg Mitrofanov <oleg@wryway.com>
2022-09-25 20:46:24 -0700

    Add transliterate.sh

---
+++ transliterate.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/env bash
+
+# Usage: ./transliterate.sh FILENAME
+# The script will convert all cyrillic letters to latin equivalents in the file.
+
+declare -A dictionary=(
+    ["а"]="a"
+    ["б"]="b"
+    ["в"]="v"
+    ["г"]="g"
+    ["д"]="d"
+    ["е"]="e"
+    ["ё"]="yo"
+    ["ж"]="zh"
+    ["з"]="z"
+    ["и"]="i"
+    ["й"]="j"
+    ["к"]="k"
+    ["л"]="l"
+    ["м"]="m"
+    ["н"]="n"
+    ["о"]="o"
+    ["п"]="p"
+    ["р"]="r"
+    ["с"]="s"
+    ["т"]="t"
+    ["у"]="u"
+    ["ф"]="f"
+    ["х"]="h"
+    ["ц"]="ts"
+    ["ч"]="ch"
+    ["ш"]="sh"
+    ["щ"]="sh"
+    ["ъ"]=""
+    ["ы"]="y"
+    ["ь"]=""
+    ["э"]="e"
+    ["ю"]="yu"
+    ["я"]="ya"
+    
+    ["А"]="A"
+    ["Б"]="B"
+    ["В"]="V"
+    ["Г"]="G"
+    ["Д"]="D"
+    ["Е"]="E"
+    ["Ё"]="Yo"
+    ["Ж"]="Zh"
+    ["З"]="Z"
+    ["И"]="I"
+    ["Й"]="J"
+    ["К"]="K"
+    ["Л"]="L"
+    ["М"]="M"
+    ["Н"]="N"
+    ["О"]="O"
+    ["П"]="P"
+    ["Р"]="R"
+    ["С"]="S"
+    ["Т"]="T"
+    ["У"]="U"
+    ["Ф"]="F"
+    ["Х"]="H"
+    ["Ц"]="Ts"
+    ["Ч"]="Ch"
+    ["Ш"]="Sh"
+    ["Щ"]="Sh"
+    ["Ъ"]=""
+    ["Ы"]="Y"
+    ["Ь"]=""
+    ["Э"]="E"
+    ["Ю"]="Yu"
+    ["Я"]="Ya"
+)
+
+# Expecting file path as the first and only argument.
+file=$1
+
+for letter in "${!dictionary[@]}"; do
+    sed -i -e "s/${letter}/${dictionary[$letter]}/g" $file
+done
